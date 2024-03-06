9e173b7e8430d10bcf9c39993a6b0e990cff3e8b
D-MERLO <delmar.5702@gmail.com>
2024-03-05 21:40:49 -0300

    M02 act2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,5 +1,33 @@
-const [tempData] = require("./tempData")
 
-const pelisContenedor = document.getElementById('pelisContenedor');
+const dataPeliculas = () => {
+    $.get("https://students-api.2.us-1.fl0.io/movies", (data, status) => {
+
+        const pelisContenedor = document.getElementById('pelisContenedor');
+
+        function renderPelis(pelicula) {
+
+            const tarjetaPelicula = document.createElement("div");
+            tarjetaPelicula.classList.add("peliCard");
+
+            tarjetaPelicula.innerHTML = `
+            <img src="${pelicula.poster}" alt="${pelicula.title}">
+            <h3>${pelicula.title} (${pelicula.year})</h3>
+            <p>${pelicula.director}</p>
+            <p>${pelicula.duration}</p>
+            <p>${pelicula.genre}</p>
+            <p>${pelicula.rate}</p>
+            `
+
+            return tarjetaPelicula;
+        }
+
+        data.forEach(pelicula => {
+            const tarjeta = renderPelis(pelicula);
+            pelisContenedor.appendChild(tarjeta);
+        });
+    })
+}
+
+
 
 
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -31,4 +31,4 @@ const tempData = [
   },
 ];
 
-module.exports = tempData;
+

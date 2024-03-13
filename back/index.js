5a7a9b10f8bedaad5077b98f44565b96139c80cf
D-MERLO <delmar.5702@gmail.com>
2024-03-13 13:15:50 -0300

    M2 act7

--- back/index.js
+++ back/index.js
@@ -5,8 +5,8 @@ app.listen(PORT, ()=>{
     console.log(`Servidor escuchando en el puerto ${PORT}`)
 })
 
-app.get("/movies", (req, res)=>{
-   res.send("Próximamente estarán disponibles los datos de películas");
-})
+// app.get("/movies", (req, res)=>{
+//    res.send("Próximamente estarán disponibles los datos de películas");
+// })
 
 module.exports = app;
--- back/src/server.js
+++ back/src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const router = require("./rutas/");
 
 const app = express();
+app.use(router);
 
 module.exports = app;

61fd228d493d63a9f0037aaf6fbab693d5b93902
D-MERLO <delmar.5702@gmail.com>
2024-03-13 10:10:45 -0300

    M02 act7

--- back/index.js
+++ back/index.js
@@ -1,3 +1,12 @@
 const app = require("./src/server");
-//y ejecutar su método listening, pasando los argumentos correspondientes.
-app.listen(3000, ()=>{})
+
+const PORT = 3000
+app.listen(PORT, ()=>{
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
+
+app.get("/movies", (req, res)=>{
+   res.send("Próximamente estarán disponibles los datos de películas");
+})
+
+module.exports = app;
